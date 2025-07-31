10e68cde033524ef7dea0d6140d157b2016d5488
s-di-cola <admin@b-side.dev>
2025-07-31 09:03:19 +0100

    feat: add error handling

--- basic-banking-system/src/bank.rs
+++ basic-banking-system/src/bank.rs
@@ -1,3 +1,19 @@
+/// Basic Banking System
+/// A simple banking system implementation in Rust demonstrating traits, structs, and method implementations.
+/// Allows creating bank accounts, depositing money, withdrawing money, and checking balances.
+/// # Example
+/// ```
+/// let mut account = BankAccount {
+///     balance: 1000,
+///     account_number: 123456,
+///     holder_name: "John Doe".to_string(),
+/// };
+///
+/// account.deposit(500).unwrap();
+///
+/// assert_eq!(account.balance, 1500);
+/// ```
+
 pub struct BankAccount {
     pub balance: i32,
     pub account_number: u32,
@@ -5,18 +21,34 @@ pub struct BankAccount {
 }
 
 pub trait Account {
-    fn deposit(&mut self, amount: i32);
-    fn withdraw(&mut self, amount: i32);
+    fn deposit(&mut self, amount: i32) -> Result<(), String>;
+    fn withdraw(&mut self, amount: i32) -> Result<(), String>;
     fn balance(&self) -> i32;
 }
-
 impl Account for BankAccount {
-    fn deposit(&mut self, amount: i32) {
-        self.balance += amount;
+    fn deposit(&mut self, amount: i32) -> Result<(), String> {
+        match amount {
+            amount if amount < 0 => Err("Cannot deposit a negative amount".to_string()),
+            amount if amount > 250000 => Err("Cannot deposit more than 250000".to_string()),
+            _ => {
+                self.balance += amount;
+                Ok(())
+            }
+        }
     }
 
-    fn withdraw(&mut self, amount: i32) {
-        self.balance -= amount;
+    fn withdraw(&mut self, amount: i32) -> Result<(), String> {
+        match amount {
+            amount if amount < 0 => Err("Cannot withdraw a negative amount".to_string()),
+            amount if amount > 250000 => Err("Cannot withdraw more than 250000".to_string()),
+            amount if self.balance < amount => {
+                Err("Cannot withdraw more than current balance".to_string())
+            }
+            _ => {
+                self.balance -= amount;
+                Ok(())
+            }
+        }
     }
 
     fn balance(&self) -> i32 {
--- basic-banking-system/src/main.rs
+++ basic-banking-system/src/main.rs
@@ -8,15 +8,27 @@ fn main() {
         holder_name: String::from("Simone Di Cola"),
     };
 
-    let mut account_maggi: BankAccount = BankAccount{
+    let mut account_maggi: BankAccount = BankAccount {
         balance: 50000000,
         account_number: 654321,
         holder_name: String::from("Silvia Maggi"),
     };
 
-    account_di_cola.deposit(39999);
-    account_maggi.withdraw(9999);
+    match account_di_cola.deposit(39999) {
+        Ok(()) => println!(
+            "Successfully deposited to {}",
+            account_di_cola.account_number
+        ),
+        Err(msg) => println!("Error while depositing: {}", msg),
+    }
+
+    account_maggi.withdraw(300000).unwrap_or_else(|err| {
+        println!("Error while withdrawing from  {}", err);
+    });
 
     println!("Simone's account balance is {}", account_di_cola.balance);
-    println!("Silvia Maggi's account balance is {}", account_maggi.balance);
+    println!(
+        "Silvia Maggi's account balance is {}",
+        account_maggi.balance
+    );
 }
